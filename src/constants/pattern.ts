0113d2e23b71d1eb5aca78d330a3dd2cb43b02f6
lixinyao0722 <502383519@qq.com>
2019-05-10 01:44:44 +0800

    fix style pattern

--- src/constants/pattern.ts
+++ src/constants/pattern.ts
@@ -1,5 +1,5 @@
 export const Pattern = {
-  STYLE: /<style\s+.*lang="scss"\s+.*>([\S\s]+)<\/style>/,
+  STYLE: /<style\s+.*lang="scss"\s*.*>([\S\s]+)<\/style>/,
   COLOR: /(#([\da-z]{6}|[\da-z]{3})|rgb\s*\((\s*(25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)\s*,?){3}\s*\))/ig,
   VAR_2_CONSTS: /\$[\w-]+\s*:\s*(#([\da-z]{6}|[\da-z]{3})|rgb\s*\((\s*(25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)\s*,?){3}\s*\))\s*;/g,
 };
